6027a8fc6b0b8ee7296f77dd9f7d28b3ae98e57c
mallj2392 <mallj2392@FHC-IC0034101.wrdsb.ca>
2015-01-08 10:14:58 -0500

    boooooooooooooooooobs

---
+++ WTAproject/src/CardLayoutTest/WTAlaunch.java
@@ -0,0 +1,60 @@
+package CardLayoutTest;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;  // Packages used in this program imported    
+
+public class WTAlaunch extends JFrame 
+{
+
+    JPanel panMaster;
+    CardLayout cardLayout;
+
+    public WTAlaunch() //This is the CONSTRUCTOR method
+    {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ActionListener buttonListener = new ButtonListener(); // defined below
+        setLayout(new FlowLayout()); //Use this for now.
+        setSize(810, 510); //Set the size of the JFrame
+        setTitle("Generic Card Game"); //Put Title on top of JFrame
+        setBackground(Color.yellow);
+        setResizable(false);
+        
+        PanOne panOne = new PanOne(buttonListener);
+        PanTwo panTwo = new PanTwo(buttonListener);
+        PanThree panThree = new PanThree(buttonListener);
+
+        panMaster = new JPanel(new CardLayout());
+
+        panMaster.add(panOne);
+        panMaster.add(panTwo);
+        panMaster.add(panThree);
+        getContentPane().add(panMaster); 
+        setVisible(true); //Make JFrame visible
+        cardLayout = (CardLayout) panMaster.getLayout();
+        
+    }
+    class ButtonListener implements ActionListener{
+        
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Button is pressed");
+            String sAction;
+            sAction = e.getActionCommand();
+            if(sAction.equals("Next")){
+                cardLayout.next(panMaster);
+                
+            }
+            if(sAction.equals("Previous")){
+                cardLayout.previous(panMaster);
+            }
+        }
+        
+    }
+    
+    public static void main(String args[])
+    {    
+        WTAlaunch cardLayoutTest = new WTAlaunch(); // This calls the constructor and runs it    
+    }
+}
