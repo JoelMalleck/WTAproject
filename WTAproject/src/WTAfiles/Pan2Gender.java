5198b7441cc1c639b48ee35610f9882d46af5437
mallj2392 <mallj2392@FHC-IC0034101.wrdsb.ca>
2015-01-09 09:45:27 -0500

    yeah buddy

--- WTAproject/src/WTAfiles/Pan2Gender.java
+++ WTAproject/src/WTAfiles/Pan2Gender.java
@@ -9,20 +9,36 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class Pan2Gender extends JPanel{
     JButton btnNext, btnPrev;
     JLabel lblId;
+    JRadioButton Male0, Female1;
     
     public Pan2Gender(ActionListener buttonListener){
-        lblId = new JLabel("This is panel #3");
+        lblId = new JLabel("Choose your gender!");
         btnNext = new JButton("Next");
         btnPrev = new JButton("Previous");
         add(lblId);
         add(btnNext);
         add(btnPrev);
         btnNext.addActionListener(buttonListener);
-        btnPrev.addActionListener(buttonListener);
+        btnPrev.addActionListener(buttonListener);      
+    }
+    public Pan2Gender2(ActionListener digFinder){
+        lblId = new JLabel("Choose your gender!");
+        btnNext = new JButton("Next");
+        btnPrev = new JButton("Previous");
+        Male0 = new JRadioButton("Male");
+        Female1 = new JRadioButton("Female");
+        add(lblId);
+        add(Male0);
+        add(Female1);
+        add(btnNext);
+        add(btnPrev);
+        
+        Male0.addActionListener(digFinder);
     }
     
     
